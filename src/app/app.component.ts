1ad6fe1740245c2bb857a99f3a309e3d7e0c8196
Fernando Vargas <fernandovargas@uees.edu.ec>
2024-06-04 08:30:20 -0500

    Taller_Angular2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,25 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContactoComponent } from './shared/contacto/contacto.component';
 import { ListaPersonasComponent } from './shared/lista-personas/lista-personas.component';
+import { RecursosService } from './servicios/recursos.service';
+import { HttpClientModule } from '@angular/common/http'; 
+import { Foto } from './interfaz/foto';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContactoComponent, ListaPersonasComponent],
+  imports: [RouterOutlet, ContactoComponent, ListaPersonasComponent, HttpClientModule],
+  providers:[RecursosService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'clienteAngular';
+  fotos: Foto[] = [];
+  constructor(private recursosServices: RecursosService){
+    recursosServices.obtenerDatos().subscribe(respuesta =>{
+      this.fotos = respuesta as Array<Foto>
+    })
+  }
 }
